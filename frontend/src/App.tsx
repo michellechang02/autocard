731c3a502819a98cf62b7c3277391a44201c87ce
michellechang02 <changmi@seas.upenn.edu>
2024-10-30 23:18:09 -0400

    tailwind + nextui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from '@nextui-org/react'
+
+function App() {
+
+  return (
+    <>
+    <div className="flex items-center justify-center h-screen">
+      <Button>Click</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // Include pages directory
+    "./components/**/*.{js,ts,jsx,tsx}", // Include components directory
+    "./app/**/*.{js,ts,jsx,tsx}", // Include app directory (if using App Router in Next.js 13+)
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}", // Include NextUI themes
+    "./node_modules/@nextui-org/react/**/*.{js,ts,jsx,tsx}", // Include NextUI components
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+}
+
